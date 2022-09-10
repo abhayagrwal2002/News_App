c9684847a8dc74fdfbc6b20cc282bf2810677a21
Abhay Singhal <abhayagrwal2002@gmail.com>
2022-09-10 16:46:51 +0530

    initial commit

---
+++ src/components/Newsitem.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class Newsitem extends Component {
+  render() {
+    let { title, description, imgUrl, newsUrl, author, date} = this.props;
+    return (
+      <div>
+        <div className="card">
+        
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted"><strong>&rarr;</strong> {!author ? "unkonown" : author}  {new Date(date).toGMTString()}</small>
+            </p>
+            <a href={newsUrl} rel="noopener noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Newsitem;
